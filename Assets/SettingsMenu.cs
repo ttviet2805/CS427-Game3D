ece6832d601e8dedcfab4335564e6b001271bb4e
tungkhongthichcode <21125171@student.hcmus.edu.vn>
2024-07-21 18:38:53 +0700

    setting

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public TMP_Dropdown graphicsDropdown;
+
+
+    public void changeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
