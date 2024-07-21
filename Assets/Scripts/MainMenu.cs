77a5e397663f2acfb07aaae8da0eac9759256b80
nghia051 <trungnghia05123@gmail.com>
2024-07-21 16:53:03 +0700

    add menu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load the next scene in the build index
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoToSettingsMenu()
+    {
+        // Load the settings scene
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void GoToMainMenu()
+    {
+        // Load the main menu scene
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        // Quit the game
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PickupSpawner.cs
+++ Assets/Scripts/PickupSpawner.cs
@@ -7,8 +7,8 @@ public class PickupSpawner : MonoBehaviour
     [SerializeField] private GameObject goldCoin, healthGlobe;
     public void DropItems()
     {
-        int num = Random.Range(1, 3);
-        if (num == 1)
+        int num = Random.Range(1, 5);
+        if (num <= 3)
             Instantiate(goldCoin, transform.position, Quaternion.identity);
         else
             Instantiate(healthGlobe, transform.position, Quaternion.identity);
