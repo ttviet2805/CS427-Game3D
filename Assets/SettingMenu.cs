996976df86aa8271aedb8b98e4c357ec3bfcee3f
tungkhongthichcode <21125171@student.hcmus.edu.vn>
2024-07-22 21:01:41 +0700

    setting (audio and quality)

--- Assets/SettingMenu.cs
+++ Assets/SettingMenu.cs
@@ -1,30 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 using TMPro;
 
-public class SettingsMenu : MonoBehaviour
+public class SettingMenu : MonoBehaviour
 {
     public TMP_Dropdown graphicsDropdown;
-
+    public Slider musicVol;
+    public AudioMixer mainAudioMixer;
     public void ChangeGraphicsQuality()
     {
         QualitySettings.SetQualityLevel(graphicsDropdown.value);
     }
-    public void changeGraphicsQuality()
+
+    public void ChangeMusicVolume()
     {
-        
+        mainAudioMixer.SetFloat("MusicVolume", musicVol.value);
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        graphicsDropdown.value = QualitySettings.GetQualityLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
